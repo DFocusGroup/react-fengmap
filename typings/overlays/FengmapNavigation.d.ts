b02bba091e0f564aedbbabda1e0e3b5d3e10662d
Alex.Zeng <Alexzeng@dfocuspace.com>
2019-02-12 15:29:09 +0800

    feat: update typings for fengmap navigation

--- typings/overlays/FengmapNavigation.d.ts
+++ typings/overlays/FengmapNavigation.d.ts
@@ -1,6 +1,22 @@
+import { coord } from './FengmapNavigation.d'
 import * as React from 'react'
 import * as fengmap from 'fengmap'
 import { Fengmap } from '../FengmapBase'
+
+export interface FengmapNavigationPointOptions {
+  x: number
+  y: number
+  groupID: number
+  url: string
+  size: number
+  callback: Function
+}
+
+export interface Coord {
+  x: number
+  y: number
+  groupID: number
+}
 
 export interface FengmapNavigationProps {
   naviOptions: {
@@ -13,29 +29,15 @@ export interface FengmapNavigationProps {
     lineStyle: Object
   }
   start: {
-    options: {
-      x: number
-      y: number
-      groupID: number
-      url: string
-      size: number
-      callback: Function
-    }
+    options: FengmapNavigationPointOptions
     noMarker: boolean
   }
   end: {
-    options: {
-      x: number
-      y: number
-      groupID: number
-      url: string
-      size: number
-      callback: Function
-    }
+    options: FengmapNavigationPointOptions
     noMarker: boolean
   }
   events: FengmapNavigationEvents
-  onDrawComplete: (map: Fengmap) => void
+  onDrawComplete: (navi: FMNavigation) => void
 }
 
 export interface FengmapNavigationState {}
@@ -53,4 +55,47 @@ export interface FengmapNavigationEvents {
   complete: EventCallback
   crossGroup: EventCallback
   walking: EventCallback
+}
+
+export interface FMNavigation {
+  clearAll(): void
+  drawNaviLine(): void
+  focusNaviLineSegment(index: int, options: { duration: number; callback: Function }): void
+  setStartPoint(options: FengmapNavigationPointOptions, noMarker: boolean): void
+  setEndPoint(options: FengmapNavigationPointOptions, noMarker: boolean): void
+  simulate(): void
+  stop(): void
+  locate(coord: Coord, angle?: number): void
+  locateNoConstraint(coord: Coord, cc: any, angle: number): void
+  naviConstraint(coord: Coord): void
+  pathConstraint(coord: Coord): void
+  naviDescriptions: Array<string>
+  naviDescriptionsData: Array<any>
+  naviDistance: number
+  naviGroupsDistance: any
+  startMarker: {
+    x: number
+    y: number
+    size: number
+    height: number
+    url: string
+    callback(): void
+  }
+  endMarker: {
+    x: number
+    y: number
+    size: number
+    height: number
+    url: string
+    callback(): void
+  }
+  locationMarker: {
+    x: number
+    y: number
+    groupID: number
+    size: number
+    height: number
+    url: string
+    callback(): void
+  }
 }
